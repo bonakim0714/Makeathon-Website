c54f97c6c8c188f903e43e8f64a91c4599198a4d
Bona Kim <bkim12@risd.edu>
2018-01-26 14:24:52 -0500

    Add files via upload

---
+++ p5/empty-example/orangesketch.js
@@ -0,0 +1,37 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background('#101010');
+}
+var style = 0;
+
+function draw() {
+  fill(random(255),random(255),random(255),50);
+  stroke(255,90);
+
+
+	// if (mouseIsPressed) {
+	// 	fill(random(255),random(255),random(255),50)
+	// } else{
+	// 	fill('#101010')
+	// 	stroke(255,80);
+	// }
+
+  if(style == 0){
+  } else if (style == 1){
+     fill('#101010');
+   }
+
+       ellipse(mouseX, mouseY, 120, 120);
+}
+
+function mouseClicked(){
+  if(style == 0){
+    style = 1;
+  } else if (style == 1){
+    style = 0; //makes it not so colorful
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight,[noRedraw]);
+}
